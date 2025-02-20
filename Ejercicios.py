77fee41244cbf8fd793e10207abdf4a6135ce2ff
UP240226 <up240226@gmail.com>
2025-02-20 11:57:37 -0600

    Day 2

---
+++ Ejercicios.py
@@ -0,0 +1,13 @@
+first_name = 'Akinori'
+last_name = 'Ono'
+country = 'Mexico'
+city = 'Aguascalientes'
+age = 18
+is_married = False
+skills = ['Python']
+person_info = {
+    'firstname': 'Akinori',
+    'lastname': 'Ono',
+    'country': 'Mexico',
+    'city': 'Aguascalientes',
+    }
---
+++ day_2 variables.py
@@ -0,0 +1,16 @@
+#'Day 2:30 Days of python programming'
+
+#Exercise 1
+first_name = 'Akinori'
+last_name = 'Ono'
+full_name = first_name + ' ' + last_name
+country = 'Mexico'
+city = 'Aguascalientes'
+age = 18
+year = 2025
+is_married = False
+is_true = True
+is_light_on = False
+print(first_name, last_name, country, city, age, year, ',', 'Is married?', is_married, ',', 'Is light on?', is_light_on)
+
+#Exercise 2
