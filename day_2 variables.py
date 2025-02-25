ddf1e0acc36827f20bd62a224607f5e1fc1e2e72
UP240226 <up240226@gmail.com>
2025-02-25 09:44:20 -0600

    Avance

--- day_2 variables.py
+++ day_2 variables.py
@@ -1,6 +1,6 @@
+#Exercise Level 1
 #'Day 2:30 Days of python programming'
 
-#Exercise 1
 first_name = 'Akinori'
 last_name = 'Ono'
 full_name = first_name + ' ' + last_name
@@ -13,4 +13,23 @@ is_true = True
 is_light_on = False
 print(first_name, last_name, country, city, age, year, ',', 'Is married?', is_married, ',', 'Is light on?', is_light_on)
 
-#Exercise 2
+#Exercise Level 2
+
+type(first_name)
+len(first_name) / len(last_name)
+
+num_one = 5
+num_two = 4
+varable_total = num_one + num_two
+variable_diff = num_one - num_two
+variable_product = num_one * num_two
+variable_division = num_one / num_two
+variable_remainder = num_one % num_two
+variable_exp = num_one ** num_two
+variable_floor_division = num_one // num_two
+print('varable_total',varable_total,',','variable_diff',variable_diff,',', 'variable_product',variable_product,',', 'variable_division',variable_division,',', 'variable_remainder',variable_remainder,',', 'variable_exp',variable_exp,',', 'variable_floor_division',variable_floor_division)
+radius = 30 
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+input(radius)
+print(area_of_circle)
---
+++ day_3 ejercicios.py
@@ -0,0 +1,19 @@
+edad = 18
+estatura = 1.59
+numero_complejo = 3 + 5j
+print(type(edad))
+print(type(estatura))
+print(type(numero_complejo))
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle:"))
+area = .5 * base * height
+print("The area of the riangle is:")
+print(area)
+
+a = float(input("Enter the side a of the triangle: "))
+b = float(input("Enter the side b of the triangle: "))     
+c = float(input("Enter the side c of the triangle: "))
+perimeter = a + b + c
+print("The perimeter of the triangle is: ")
+print(perimeter)
