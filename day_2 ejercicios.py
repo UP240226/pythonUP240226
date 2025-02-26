d47f9ea77316703f73e757a5c503f7abfb7e0860
UP240226 <up240226@gmail.com>
2025-02-26 09:42:54 -0600

    avance

--- day_2 ejercicios.py
+++ day_2 ejercicios.py
@@ -18,8 +18,8 @@ print(first_name, last_name, country, city, age, year, ',', 'Is married?', is_ma
 type(first_name)
 len(first_name) / len(last_name)
 
-num_one = 5
-num_two = 4
+num_one = float(input('Enter the first number: '))
+num_two = float(input('Enter the second number: '))
 varable_total = num_one + num_two
 variable_diff = num_one - num_two
 variable_product = num_one * num_two
@@ -27,9 +27,10 @@ variable_division = num_one / num_two
 variable_remainder = num_one % num_two
 variable_exp = num_one ** num_two
 variable_floor_division = num_one // num_two
-print('varable_total',varable_total,',','variable_diff',variable_diff,',', 'variable_product',variable_product,',', 'variable_division',variable_division,',', 'variable_remainder',variable_remainder,',', 'variable_exp',variable_exp,',', 'variable_floor_division',variable_floor_division)
-radius = 30 
+print('Suma',varable_total,',','Resta',variable_diff,',', 'Multiplicación',variable_product,',', 'División',variable_division,',', 'Porcentaje',variable_remainder,',', 'Exponencial',variable_exp,',', 'Cociente',variable_floor_division)
+
+#circulo
+radius = float(input("Enter the radius of the circle: ")) 
 area_of_circle = 3.14 * radius ** 2
 circum_of_circle = 2 * 3.14 * radius
-input(radius)
-print(area_of_circle)
+print("Area of the Circle", area_of_circle)
--- day_3 ejercicios.py
+++ day_3 ejercicios.py
@@ -17,3 +17,17 @@ c = float(input("Enter the side c of the triangle: "))
 perimeter = a + b + c
 print("The perimeter of the triangle is: ")
 print(perimeter)
+
+lenght = float(input("Dame el largo del rectángulo: "))
+width = float(input("Dame el ancho del rectángulo: "))
+area = lenght * width
+perimeter = 2 * (lenght + width)
+print("El área del rectángulo es: ", area)
+print("El perímetro del rectángulo es: ", perimeter)
+
+radius = float(input("Dame el radio del circulo: "))
+area = 3.14 * radius ** 2
+circumference = 2 * 3.14 * radius   
+print("El área del circulo es: ", area)
+print("La circunferencia del circulo es: ", circumference)
+
