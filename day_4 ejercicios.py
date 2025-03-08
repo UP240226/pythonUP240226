47a90289bf4198bb7f0e27bcf571de60c2666932
UP240226 <up240226@gmail.com>
2025-03-07 23:18:11 -0600

    Dia4

---
+++ day_4 ejercicios.py
@@ -0,0 +1,44 @@
+#Ejercicio 1
+'Thirty', 'Days', 'Of', 'Python'
+full_text = 'Thirty' + ' ' + 'Days' + ' ' + 'Of' + ' ' + 'Python'
+print(full_text)
+#Ejercicio 2
+'Coding', 'For', 'All'
+full_text = 'Coding' + ' ' + 'For' + ' ' + 'All'
+print(full_text)
+#Ejercicio 3
+company = 'Coding For All'
+str(company)
+#Ejercicio 4
+print(company)
+#Ejercicio 5
+print(len(company))
+#Ejercicio 6
+print(company.upper())
+#Ejercicio 7
+print(company.lower())
+#Ejercicio 8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+#Ejercicio 9
+cut = company[0:6]
+print(cut)
+#Ejercicio 10
+print(company.find('Coding'))
+#Ejercicio 11
+python = (company.replace('Coding', 'Python'))
+print(python)
+#Ejercicio 12
+print(python.replace('All', 'Everyone'))
+#Ejercicio 13
+print(company.split('.'))
+#Ejercicio 14
+companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(companies.split(', '))
+#Ejercicio 15
+print(company.index)
+#Ejercicio 16
+
+#Ejercicio 17
+print(company.index(10))
