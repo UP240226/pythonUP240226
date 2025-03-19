66e5584807b41f19a48991a731dbb3bb7a4ac852
UP240226 <up240226@gmail.com>
2025-03-19 10:40:19 -0600

    dia6

---
+++ 06_Day/6_Day.py
@@ -0,0 +1,17 @@
+#Ejercicio 1
+tpl = ()
+print(tpl)
+#Ejercicio 2
+tuplesister = 'Naomi', 'Alexa', 'Sofia'
+tuplebrother = 'Satoshi', 'Marco', 'Jorge'
+#Ejercicio 3
+siblings = (tuplesister) + (tuplebrother)
+print('Siblings:', siblings)
+#Ejercicio 4
+lensib = len(siblings)
+print('How many siblings do you have:', lensib)
+#Ejercicio 5
+father = siblings.append('Victor')
+mother = siblings.append('Eli')
+family_members = siblings
+print('Family members:', family_members)
