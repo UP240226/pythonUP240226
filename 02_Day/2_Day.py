6f5d7705d8da4499d89522b9e351b09fa39bc444
UP240226 <up240226@gmail.com>
2025-03-26 10:40:18 -0600

    7

--- 02_Day/2_Day.py
+++ 02_Day/2_Day.py
@@ -1,30 +1,66 @@
 #Exercise Level 1
+print('Exercises: Level 1')
+print("")
 #Day 2: 30 Days of python programming
 #Ejercicio 3
+print('Ejercicio 3')
 first_name = 'Akinori'
+print(first_name)
+print("")
 #Ejercicio 4
+print('Ejercicio 4')
 last_name = 'Ono'
+print(last_name)
+print("")
 #Ejericio 5
+print('Ejercicio 5')
 full_name = first_name + ' ' + last_name
+print(full_name)
+print("")
 #Ejercicio 6
+print('Ejercicio 6')
 country = 'Mexico'
+print(country)
+print("")
 #Ejercicio 7
+print('Ejercicio 7')
 city = 'Aguascalientes'
+print(city)
+print("")
 #Ejercicio 8
+print('Ejercicio 8')
 age = 18
+print(age)
+print("")
 #Ejercicio 9
+print('Ejercicio 9')
 year = 2025
+print(year)
+print("")
 #Ejercicio 10
+print('Ejercicio 10')
 is_married = False
+print(is_married)
+print("")
 #Ejercicio 11
+print('Ejercicio 11')
 is_true = True
+print(is_true)
+print("")
 #Ejercicio 12
+print('Ejercicio 12')
 is_light_on = False
+print(is_light_on)
+print("")
 #Ejercicio 13
+print('Ejercicio 13')
 print(first_name, last_name, country, city, age, year, ',', 'Is married?', is_married, ',', 'Is light on?', is_light_on)
-
+print("")
 #Exercise Level 2
+print('Exercises: Level 2')
+print("")
 #Ejercicio 1
+print('Ejercicio 1')
 type(first_name)
 type(last_name)
 type(full_name)
@@ -35,7 +71,10 @@ type(year)
 type(is_married)
 type(is_true)
 type(is_light_on)
+print('Types:', type(first_name), type(last_name), type(full_name), type(country), type(city), type(age), type(year), type(is_married), type(is_true), type(is_light_on))
+print("")
 #Ejercicio 2
+print('Ejercicio 2')
 len(first_name)
 len(last_name)
 len(full_name)
@@ -43,15 +82,30 @@ len(country)
 len(city)
 len(str(age))
 len(str(year))
+print('Length:', len(first_name), len(last_name), len(full_name), len(country), len(city), len(str(age)), len(str(year)))
+print("")
 #Ejercicio 3
+print('Ejercicio 3')
 Diference = len(first_name) - len(last_name)
+print('Diference:', Diference)
+print("")
 #Ejercicio 4
+print('Ejercicio 4')
 num_one = 5
 num_two = 4
+print('First number:', num_one)
+print('Second number:', num_two)
+print("")
 #Ejercicio 5
+print('Ejercicio 5')
 total = num_one + num_two
+print('Total:', total)
+print("")
 #Ejercicio 6
+print('Ejercicio 6')
 diff = num_one - num_two
+print('Difference:', diff)
+print("")
 #Ejercicio 7
 product = num_two * num_one
 #Ejercicio 8
---
+++ 07_Day/7_Day.py
@@ -0,0 +1,104 @@
+#Exercises: Level 1
+print('Exercises: Level 1')
+print("")
+#Ejercicio 1
+print('Ejercicio 1')
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+len(it_companies)
+print('Companies:', it_companies)
+print('Number of companies:', len(it_companies))
+print("")
+#Ejercicio 2
+print('Ejercicio 2')
+it_companies.add('Twitter')
+print('Add Twitter', it_companies)
+print("")
+#Ejercicio 3
+print('Ejercicio 3')
+it_companies.update(['Tesla', 'SpaceX', 'IBM'])
+print('More Compaies', it_companies)
+print("")
+#Ejercicio 4
+print('Ejercicio 4')
+it_companies.remove('IBM')
+print('Remove IBM', it_companies)
+print("")
+#Ejercicio 5
+print('Ejercicio 5')
+it_companies.discard('SpaceX')
+print('Discard SpaceX', it_companies)
+print('The diference between remove and discard is that remove will raise an error if the item does not exist in the set, while discard will not raise an error')
+print("")
+
+#Exercises: Level 2
+print('Exercises: Level 2')
+print("")
+#Ejercicio 1
+print('Ejercicio 1')
+A = {19, 22, 24, 20, 25, 26}
+B = {18, 17, 24, 22, 23, 27}
+print('A:', A)
+print('B:', B)
+A.union(B)
+print('Union:', A.union(B))
+print("")
+#Ejercicio 2
+print('Ejercicio 2')
+find = A.intersection(B)
+print('Intersection:', find)
+print("")
+#Ejercicio 3
+print('Ejercicio 3')
+subset = A.issubset(B)
+print('Is A subset of B?:', subset)
+print("")
+#Ejercicio 4
+print('Ejercicio 4')
+disjoint = A.isdisjoint(B)
+print('Are A and B disjoint?:', disjoint)
+print("")
+#Ejercicio 5
+print('Ejercicio 5')
+join = A.union(B)
+inv = B.union(A)
+print('Join:', join)
+print('Inverse:', inv)
+print("")
+#Ejercicio 6
+print('Ejercicio 6')
+A.symmetric_difference(B)
+print('Symmetric Difference:', A.symmetric_difference(B))
+print("")
+#Ejercicio 7
+print('Ejercicio 7')
+del A
+del B
+print('Del A:')
+print('Del B:')
+print("")
+
+#Exercises: Level 3
+print('Exercises: Level 3')
+print("")
+#Ejercicio 1
+print('Ejercicio 1')
+ages = [18, 45, 15, 23, 44, 32, 22, 19, 24, 25, 26, 24, 25, 24]
+set_ages = set(ages)
+print(set_ages)
+list = len(ages)
+st = len(set_ages)
+print('Lenght list', list)
+print('Lenght set', st)
+print("")
+#Ejercicio 2
+print('Ejercicio 2')
+print('An string is')
+print('A list is')
+print('A tuple is')
+print('A set is')
+print("")
+#Ejercicio 3
+print('Ejercicio 3')
+sentence = 'I am a teacher and I love to inspire and teach people'
+unique = {'teacher', 'love', 'inspire'}
+split = unique.split(sentence)
